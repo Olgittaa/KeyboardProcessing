48426b3c6d9a4cb41608eee9a164d2886160fa6b
olga_charna <olya13771@gmail.com>
2022-10-03 15:04:09 +0200

    Initial Commit

---
+++ SpracovanieKlaves/AppDelegate.swift
@@ -0,0 +1,24 @@
+//
+//  AppDelegate.swift
+//  SpracovanieKlaves
+//
+//  Created by Ольга Чарная on 03.10.2022.
+//
+
+
+import Cocoa
+
+@main
+class AppDelegate: NSObject, NSApplicationDelegate {
+    
+    
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+    
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+    
+    
+}
