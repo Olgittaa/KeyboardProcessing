d079d33faa39b2d637ccf5bb27e5a53e81993ed2
olga_charna <olya13771@gmail.com>
2022-11-08 18:36:35 +0100

    init

--- SpracovanieKlaves/GameScene.swift
+++ SpracovanieKlaves/GameScene.swift
@@ -1,91 +1,125 @@
-//
-//  GameScene.swift
-//  SpracovanieKlaves
-//
-//  Created by Ольга Чарная on 03.10.2022.
-//
-
 import SpriteKit
 import GameplayKit
 
 class GameScene: SKScene {
     
-    private var label : SKLabelNode?
-    private var spinnyNode : SKShapeNode?
+    var avatar: SKSpriteNode!
+    var timer: Timer!
+    var x = 0
+    var y = 0
+    var dx = 0
+    var dy = 0
+    var left = Key()
+    var right = Key()
+    var up = Key()
+    var down = Key()
     
     override func didMove(to view: SKView) {
+        avatar = SKSpriteNode(color: .red, size: CGSize(width: self.size.width/50, height: self.size.width/50))
+        addChild(avatar)
+        avatar.position = CGPoint(x: x, y: y)
         
-        // Get label node from scene and store it for use later
-        self.label = self.childNode(withName: "//helloLabel") as? SKLabelNode
-        if let label = self.label {
-            label.alpha = 0.0
-            label.run(SKAction.fadeIn(withDuration: 2.0))
-        }
-        
-        // Create shape node to use during mouse interaction
-        let w = (self.size.width + self.size.height) * 0.05
-        self.spinnyNode = SKShapeNode.init(rectOf: CGSize.init(width: w, height: w), cornerRadius: w * 0.3)
-        
-        if let spinnyNode = self.spinnyNode {
-            spinnyNode.lineWidth = 2.5
-            
-            spinnyNode.run(SKAction.repeatForever(SKAction.rotate(byAngle: CGFloat(Double.pi), duration: 1)))
-            spinnyNode.run(SKAction.sequence([SKAction.wait(forDuration: 0.5),
-                                              SKAction.fadeOut(withDuration: 0.5),
-                                              SKAction.removeFromParent()]))
-        }
+        timer = Timer.scheduledTimer(timeInterval: 0.5, target: self, selector: #selector(fireTimer), userInfo: nil, repeats: true)
     }
     
+    fileprivate func keysUpdateOnTimerTik() {
+        left.tik()
+        right.tik()
+        up.tik()
+        down.tik()
+    }
     
-    func touchDown(atPoint pos : CGPoint) {
-        if let n = self.spinnyNode?.copy() as! SKShapeNode? {
-            n.position = pos
-            n.strokeColor = SKColor.green
-            self.addChild(n)
+    fileprivate func doMove() {
+        let step = Int(avatar.size.width) / 2
+        if left.status == 1 || left.status == 2 || left.status == 3{
+            dx -= step
+        }
+        if right.status == 1 || right.status == 2 || right.status == 3{
+            dx += step
+        }
+        if up.status == 1 || up.status == 2 || up.status == 3{
+            dy += step
+        }
+        if down.status == 1 || down.status == 2 || down.status == 3{
+            dy -= step
         }
     }
     
-    func touchMoved(toPoint pos : CGPoint) {
-        if let n = self.spinnyNode?.copy() as! SKShapeNode? {
-            n.position = pos
-            n.strokeColor = SKColor.blue
-            self.addChild(n)
-        }
+    @objc func fireTimer() {
+        doMove()
+        x += dx
+        y += dy
+        moveAvatar(x: x, y: y)
+        keysUpdateOnTimerTik()
+        print(left.status)
+        dx = 0
+        dy = 0
     }
     
-    func touchUp(atPoint pos : CGPoint) {
-        if let n = self.spinnyNode?.copy() as! SKShapeNode? {
-            n.position = pos
-            n.strokeColor = SKColor.red
-            self.addChild(n)
-        }
-    }
-    
-    override func mouseDown(with event: NSEvent) {
-        self.touchDown(atPoint: event.location(in: self))
-    }
-    
-    override func mouseDragged(with event: NSEvent) {
-        self.touchMoved(toPoint: event.location(in: self))
-    }
-    
-    override func mouseUp(with event: NSEvent) {
-        self.touchUp(atPoint: event.location(in: self))
+    fileprivate func moveAvatar(x: Int, y: Int) {
+        avatar.position = CGPoint(x: x, y: y)
     }
     
     override func keyDown(with event: NSEvent) {
-        switch event.keyCode {
-        case 0x31:
-            if let label = self.label {
-                label.run(SKAction.init(named: "Pulse")!, withKey: "fadeInOut")
-            }
+        switch event.keyCode{
+        case 123:
+            left.pressed()
+        case 124:
+            right.pressed()
+        case 125:
+            down.pressed()
+        case 126:
+            up.pressed()
         default:
-            print("keyDown: \(event.characters!) keyCode: \(event.keyCode)")
+            print(event.keyCode)
+        }
+    }
+    
+    override func keyUp(with event: NSEvent) {
+        switch event.keyCode{
+        case 123:
+            left.release()
+        case 124:
+            right.release()
+        case 125:
+            down.release()
+        case 126:
+            up.release()
+        default:
+            print(event.keyCode)
         }
     }
     
     
-    override func update(_ currentTime: TimeInterval) {
-        // Called before each frame is rendered
+    
+    class Key{
+        var status = 0 // 0 - pusteny, 1 - stlaceny, 2 - impulz, 3 - drzany
+        
+        func tik(){
+            if status == 1{
+                status = 3
+            } else if status == 2{
+                status = 0
+                // pohyb
+            } else if status == 3{
+                //pohyb
+            }
+        }
+        
+        func pressed(){
+            if status == 0{
+                status = 1
+            } else if status == 2 {
+                status = 1
+            }
+        }
+        
+        func release(){
+            if status == 1{
+                status = 2
+            } else if status == 3 {
+                status = 0
+            }
+        }
     }
 }
